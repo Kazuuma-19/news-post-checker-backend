48d34c43b97885e2aec2692d73d747a61eb04b83
Kazuma Saito <kazu15870@gmail.com>
2024-01-15 10:15:31 -0800

    chore: connection test with fly.io

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
+  res.send('<p>test test</p>');
+});
+
+router.get('/', function (req, res) {
   res.json({ message: "api test" });
 });
 
