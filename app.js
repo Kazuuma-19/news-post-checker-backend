36bd4cc386faa9e73fd221667ae7ac60660f7fc1
Kazuma Saito <kazu15870@gmail.com>
2024-01-28 16:03:58 -0800

    feat: made students table and api

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const indexRouter = require('./routes/index');
+const express = require("express");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+
+app.use(express.json());
 
 app.use(indexRouter);
 
