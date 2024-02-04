a63aa51b4c5c5864eeab0a260615446fa31658f7
Kazuma Saito <kazu15870@gmail.com>
2024-02-04 13:24:48 -0800

    fix: fix error students table doesn't exist

--- prisma/migrations/20240204204931_init/migration.sql
+++ prisma/migrations/20240204204931_init/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "Students" (
+CREATE TABLE "students" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Students_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "students_pkey" PRIMARY KEY ("id")
 );
